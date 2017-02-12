e306bdd892933ed3deae20777e9b75704010c567
Martin Robinson <martin.robinson@lyit.ie>
2017-02-12 18:49:29 +0000

    Implemented new method in deck class
    So now deck has a drawAllRemainingCards methods which empties the deck
    and an accompanying Junit test.

--- CardGame/src/ie/lyit/comp/cardgame/deck.java
+++ CardGame/src/ie/lyit/comp/cardgame/deck.java
@@ -6,8 +6,7 @@ import java.util.List;
 
 public class deck {
 	int cardsInDeck = 0;
-	
-	
+
 	private List<card> deck = new ArrayList<>();
 
 	public deck() {
@@ -17,7 +16,7 @@ public class deck {
 				cardsInDeck++;
 			}
 		}
-		
+
 	}
 
 	public void printAllCards() {
@@ -25,23 +24,31 @@ public class deck {
 			System.out.println(card.toString() + " " + card.getRank());
 		}
 	}
-	
+
+	public void drawAllRemainingCards() {
+		for (card card : deck) {
+			card drawnCard = card;
+			drawnCard.toString();
+			cardsInDeck--;
+		}
+	}
+
 	@Override
 	public String toString() {
-		
+
 		return "deck [deck=" + deck + "]";
 	}
 
 	public card drawNextCard() {
-		card drawnCard =deck.get(cardsInDeck-1);
+		card drawnCard = deck.get(cardsInDeck - 1);
 		cardsInDeck--;
 		return drawnCard;
 	}
-	
+
 	public void shuffle() {
 		Collections.shuffle(deck);
 	}
-	
+
 	public int getCardCount() {
 		return cardsInDeck;
 	}
@@ -50,5 +57,12 @@ public class deck {
 		card drawnCard = deck.get(position);
 		cardsInDeck--;
 		return drawnCard;
+	}
+
+	public Boolean isEmpty() {
+		if (cardsInDeck == 0)
+			return true;
+		else
+			return false;
 	}
 }
--- CardGame/tests/ie/lyit/comp/cardgame/deckTest.java
+++ CardGame/tests/ie/lyit/comp/cardgame/deckTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 public class deckTest {
 	deck testDeck;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		testDeck = new deck();
@@ -24,11 +24,17 @@ public class deckTest {
 		String result = testCard.toString();
 		assertEquals(result, "card [suite=CLUBS, rank=KING]");
 	}
-	
+
 	@Test
 	public void testDrawnKnownCardAtPosition() {
 		card testCard = testDeck.drawCardAtPosition(10);
 		String result = testCard.toString();
-		assertEquals(result,"card [suite=SPADES, rank=JACK]" );
+		assertEquals(result, "card [suite=SPADES, rank=JACK]");
+	}
+
+	@Test
+	public void testDeckIsEmpty() {
+		testDeck.drawAllRemainingCards();
+		assertEquals(0, testDeck.getCardCount());
 	}
 }
